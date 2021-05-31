3c61797654fc32942284917a707898879b6aeacb
Gabe Aguilar <gabe.p.aguilar@gmail.com>
2021-05-30 20:10:35 -0400

    add create route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do 
       resources :neighborhoods, only: [:index]
-      resources :cocktails, only: [:index]
+      resources :cocktails, only: [:index, :create]
     end
   end
 end
